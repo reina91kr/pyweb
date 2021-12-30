619d45922c553cea88da75608e7cf7a9224b6108
reina91kr <jjangyunji@gmail.com>
2021-12-30 11:23:57 +0900

    'upload'

--- board/views/vote_views.py
+++ board/views/vote_views.py
@@ -1,24 +1,24 @@
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect
-
-from board.models import Answer, Question
+from django.shortcuts import render
+from poll.models import Question, Choice
 
 
-@login_required(login_url='common:login')
-def vote_question(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if request.user == question.author:
-        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
+def index(request):
+    poll_list = Question.objects.all()
+    context = {'poll_list': poll_list}
+
+    return render(request, 'poll/poll_list.html', context)
+
+def detail(request, pk):
+    question = Question.objects.get(id=pk)
+    return render(request, 'poll/detail.html',{'question': question})
+
+def vote(request, pk):
+    question = Question.objects.get(id=pk)
+    try:
+        sel_choice = question.choice_set.get(id=request.POST['choice'])
+    except KeyError:
+        return render(request, 'poll/detail.html', {'question': question, 'error': '선택을 확인하세요'})
     else:
-        question.voter.add(request.user)
-    return redirect('board:detail', question_id=question.id)
-
-@login_required(login_url='common:login')
-def vote_answer(request, answer_id):
-    answer = get_object_or_404(Answer, pk=answer_id)
-    if request.user == answer.author:
-        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
-    else:
-        answer.voter.add(request.user)
-    return redirect('board:detail', question_id=answer.question.id)
+        sel_choice.votes = sel_choice.votes + 1
+        sel_choice.save()
+        return render(request, 'poll/vote_result.html', {'question': question})
--- poll/urls.py
+++ poll/urls.py
@@ -1,11 +1,11 @@
-import views as views
+import board.views as views
 from django.urls import path
-from poll import views
+from board.views import vote_views
 
 app_name = 'poll'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:pk>/', views.detail, name='detail'),
-    path('<int:pk>/vote/', views.vote, name='vote'),
+    path('', vote_views.index, name='index'),
+    path('<int:pk>/', vote_views.detail, name='detail'),
+    path('<int:pk>/vote/', vote_views.vote, name='vote'),
 ]
